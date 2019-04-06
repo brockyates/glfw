dbcd00332ef1b5d38da6e99ce8db62fe0fc34c86
Brock Yates <brock.e.yates@gmail.com>
2019-04-06 14:51:11 -0700

    Fixup premake

--- premake5.lua
+++ premake5.lua
@@ -19,28 +19,32 @@ project "GLFW"
     }
 
     filter "system:windows"
-    buildoptions { "-std=c11", "-lgdi32" }
-    systemversion "latest"
-    staticruntime "On"
+        systemversion "latest"
+        staticruntime "On"
 
-    files
-    {
-        "src/win32_init.c",
-        "src/win32_joystick.c",
-        "src/win32_monitor.c",
-        "src/win32_time.c",
-        "src/win32_thread.c",
-        "src/win32_window.c",
-        "src/wgl_context.c",
-        "src/egl_context.c",
-        "src/osmesa_context.c"
-    }
+        files
+        {
+            "src/win32_init.c",
+            "src/win32_joystick.c",
+            "src/win32_monitor.c",
+            "src/win32_time.c",
+            "src/win32_thread.c",
+            "src/win32_window.c",
+            "src/wgl_context.c",
+            "src/egl_context.c",
+            "src/osmesa_context.c"
+        }
 
-    defines
-    {
-        "_GLFW_WIN32",
-        "_CRT_SECURE_NO_WARNINGS"
-    }
+        defines
+        {
+            "_GLFW_WIN32",
+            "_CRT_SECURE_NO_WARNINGS"
+        }
 
-    filter { "system:winows", "configurations.Release" }
-        buildoptions "/MT"
+    filter "configurations:Release"
+        runtime "Release"
+        optimize "On"
+
+    filter "configurations:Debug"
+        runtime "Debug"
+        symbols "On"
